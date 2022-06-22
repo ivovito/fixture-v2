3f7b9e6c0205bb8822ca8e7a0bdd4cecb00dd9c8
ivovito <92887408+ivovito@users.noreply.github.com>
2022-06-22 12:36:53 -0300

    Add files via upload

--- main1.js
+++ main1.js
@@ -7,6 +7,10 @@ let DiferenciaGol = false;
 let Puntaje = false;
 function handleSubmit(event) {
   event.preventDefault();
+  let formis = form.querySelector('label');
+  let nashe = form.element.querySelector('label')[3];
+  console.log(event.target.children[formis]);
   console.log(event.target.children[1].value);
   console.log(event.target.children[2].value);
+  console.log(event.target.children[nashe]);
 }
