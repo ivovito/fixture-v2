4e7cd6cd61a3807408a22f8f6d59a9bad9ba49ad
ivovito <92887408+ivovito@users.noreply.github.com>
2022-06-25 11:52:50 -0300

    Update main1.js
    ordenado y terminado

--- main1.js
+++ main1.js
@@ -20,39 +20,64 @@ function handleSubmit(event) {
   console.log(tds);
   for (let td of tds) {
 
-    if (td.textContent.includes(local)) {
-      if (ganoLocal) {
-      console.log(td.parentElement.children[2].textContent)
-        td.parentElement.children[2].textContent = +td.parentElement.children[2].textContent +1;
-      }else if (empate) {
+ if (td.textContent.includes(local)) {
+                                                                                                //partidos Ganados/Perdidos local
+      if (ganoLocal)  td.parentElement.children[2].textContent = +td.parentElement.children[2].textContent + 1;
+      else if (empate) ;
+      else td.parentElement.children[4].textContent = +td.parentElement.children[4].textContent + 1;
 
-      }
-        td.parentElement.children[4].textContent = +td.parentElement.children[4].textContent + 1;
-        }
+                                                                                                //Puntaje si gana local
+        if (ganoLocal) td.parentElement.children[8].textContent= +td.parentElement.children[8].textContent + 3;
 
+                                                                                                //Diferencia de gol local
+        if (ganoLocal) td.parentElement.children[7].textContent = +td.parentElement.children[7].textContent + golesLocal - golesVisitante;
+        else if (empate) ;
+        else td.parentElement.children[7].textContent = +td.parentElement.children[7].textContent + golesLocal - golesVisitante;
+
+                                                                                                //goloes en contra del local
+        if(ganoLocal) td.parentElement.children[6].textContent = +td.parentElement.children[6].textContent + +golesVisitante;
+        else if(empate) td.parentElement.children[6].textContent = +td.parentElement.children[6].textContent + +golesVisitante;
+        else td.parentElement.children[6].textContent = +td.parentElement.children[6].textContent + +golesVisitante;
+                                                                                                //Goles a favor local
+        if(ganoLocal) td.parentElement.children[5].textContent = +td.parentElement.children[5].textContent + +td.parentElement.children[7].textContent + +golesVisitante;
+        else if(empate) td.parentElement.children[5].textContent = +td.parentElement.children[5].textContent + +td.parentElement.children[7].textContent + +golesVisitante;
+        else td.parentElement.children[5].textContent = +td.parentElement.children[5].textContent + +td.parentElement.children[7].textContent + +golesVisitante;
     }
 
 if (td.textContent.includes(visitante)) {
-  if (ganoLocal) {
-    td.parentElement.children[4].textContent = +td.parentElement.children[4].textContent + 1;
-  }else if (empate) {
+                                                                                                //Partidos Ganados/Perdidos visitante
+  if (ganoLocal) td.parentElement.children[4].textContent = +td.parentElement.children[4].textContent + 1;
+  else if (empate)  ;
+  else td.parentElement.children[2].textContent = +td.parentElement.children[2].textContent + 1;
 
-  }else {
-    td.parentElement.children[2].textContent = +td.parentElement.children[2].textContent + 1;
-  }
+                                                                                                //Puntaje si gana visitante
+  if    (ganoLocal) ;
+  else if (empate)  ;
+  else td.parentElement.children[8].textContent= +td.parentElement.children[8].textContent +3;
+
+                                                                                                //Goles a favor visitante
+  if(ganoLocal) td.parentElement.children[5].textContent = +td.parentElement.children[5].textContent + +td.parentElement.children[7].textContent + +golesVisitante;
+  else if(empate) td.parentElement.children[5].textContent = +td.parentElement.children[5].textContent + +td.parentElement.children[7].textContent + +golesVisitante;
+  else td.parentElement.children[5].textContent = +td.parentElement.children[5].textContent + +td.parentElement.children[7].textContent + +golesVisitante;
+                                                                                                //Diferencia de gol visitante
+  if (ganoLocal) td.parentElement.children[7].textContent = +td.parentElement.children[7].textContent + golesVisitante - golesLocal;
+  else if (empate) ;
+  else td.parentElement.children[7].textContent = +td.parentElement.children[7].textContent + golesLocal - golesVisitante;
+
+                                                                                                //goloes en contra del visitante
+  if(ganoLocal) td.parentElement.children[6].textContent = +td.parentElement.children[6].textContent + +golesLocal;
+  else if(empate) td.parentElement.children[6].textContent = +td.parentElement.children[6].textContent + +golesLocal;
+  else td.parentElement.children[6].textContent = +td.parentElement.children[6].textContent + +golesLocal;
 }
 
     if (td.textContent.includes(local)|| td.textContent.includes (visitante)){
     console.log(td.parentElement);
-                                              //partidosGanados
+                                                                                                //Partidos Empatados local/visitante
+      if (empate)  td.parentElement.children[3].textContent = +td.parentElement.children[3].textContent + 1 ;
+                                                                                                //Puntaje para ambos equipos si empatan
+      if (empate) td.parentElement.children[8].textContent = +td.parentElement.children[8].textContent + 1;
 
-    if (empate) {
-      console.log(td.parentElement.children[3].textContent);
-      td.parentElement.children[3].textContent =
-      +td.parentElement.children[3].textContent + 1 ;
-    }
-    if (empate) {
-      td.parentElement.children[8].textContent = +td.parentElement.children[8].textContent + 1;
-    }
+
     }
   }
+}
